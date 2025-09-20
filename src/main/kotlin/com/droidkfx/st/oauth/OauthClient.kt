0b6ff1332f90f5a523d8ab78ab1f81461af36d3b
droidkfx <kam157190@gmail.com>
2025-09-20 11:49:06 -0500

    Add configuration handling with Kotlin serialization
    - Integrated `kotlinx-serialization` plugin in Gradle.
    - Added `ConfigService` to load configuration from JSON files.
    - Defined `ConfigEntity` and `SchwabClientConfig` data classes for configuration structure.
    - Removed unused `MainFrame` class and references.
    - Updated `.gitignore` to exclude sensitive configuration files.

---
+++ src/main/kotlin/com/droidkfx/st/oauth/OauthClient.kt
@@ -0,0 +1,5 @@
+package com.droidkfx.st.oauth
+
+class OauthClient {
+
+}
---
+++ src/main/kotlin/com/droidkfx/st/oauth/OauthService.kt
@@ -0,0 +1,9 @@
+package com.droidkfx.st.oauth
+
+import com.droidkfx.st.config.SchwabClientConfig
+
+class OauthService(
+    val config: SchwabClientConfig
+) {
+
+}

cc65e9c0ba382bbf2d4035913508734ad457a5ef
droidkfx <kam157190@gmail.com>
2025-09-23 13:32:53 -0500

    Add FlatLightLaf setup in `Main.kt` for consistent UI theme initialization

--- src/main/kotlin/com/droidkfx/st/Main.kt
+++ src/main/kotlin/com/droidkfx/st/Main.kt
@@ -4,8 +4,11 @@ import com.droidkfx.st.config.ConfigModule
 import com.droidkfx.st.controller.ControllerModule
 import com.droidkfx.st.oauth.OauthModule
 import com.droidkfx.st.schwab.SchwabModule
+import com.formdev.flatlaf.FlatLightLaf
 
 fun main() {
+    FlatLightLaf.setup()
+
     val configModule = ConfigModule("application.no-commit.config.json")
     val schwabModule = SchwabModule(configModule)
     val oauthModule = OauthModule(configModule, schwabModule)

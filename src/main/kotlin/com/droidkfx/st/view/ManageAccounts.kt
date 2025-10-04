60075ce99ed1b713581c2266c176a5b54154cea7
droidkfx <kam157190@gmail.com>
2025-10-04 12:10:45 -0500

    Enhance `ManageAccounts` UI with account details, position configurations, and layout improvements
    - Added `JList` to display accounts with a refresh action.
    - Implemented account detail display (ID, Hash) using `GridBagLayout`.
    - Introduced a `JTable` to showcase position allocations within a scrollable view.
    - Updated dialog initialization with minimum size and layout adjustments.

--- src/main/kotlin/com/droidkfx/st/view/ManageAccounts.kt
+++ src/main/kotlin/com/droidkfx/st/view/ManageAccounts.kt
@@ -1,13 +1,24 @@
 package com.droidkfx.st.view
 
 import io.github.oshai.kotlinlogging.KotlinLogging.logger
+import java.awt.BorderLayout
+import java.awt.Dimension
 import java.awt.Frame
+import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
+import java.util.Vector
+import javax.swing.JButton
 import javax.swing.JDialog
 import javax.swing.JLabel
+import javax.swing.JList
 import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.JTable
+import javax.swing.border.BevelBorder
+import javax.swing.border.CompoundBorder
+import javax.swing.border.EmptyBorder
 
 @Suppress("USELESS_CAST") // Cast is required for overload ambiguity
 abstract class ManageAccounts : JDialog(null as? Frame, "Manage Accounts", true) {
@@ -15,10 +26,78 @@ abstract class ManageAccounts : JDialog(null as? Frame, "Manage Accounts", true)
 
     init {
         logger.trace { "Initializing" }
+        minimumSize = Dimension(700, 500)
         add(JPanel(GridBagLayout()).apply {
-            add(JPanel().apply {
-                add(JLabel("Manage Accounts menu (WIP)"))
+            add(JPanel(BorderLayout()).apply {
+                add(JList(Vector(listOf("Account 1", "Account 2"))), BorderLayout.CENTER)
+                add(JPanel().apply {
+                    add(JButton("Refresh"))
+                    border = EmptyBorder(5, 5, 5, 5)
+                }, BorderLayout.SOUTH)
+            }, GridBagConstraints().apply {
+                gridx = 0
+                gridy = 0
+                weightx = 1.0
+                weighty = 1.0
+                fill = GridBagConstraints.BOTH
             })
+
+            add(JPanel(GridBagLayout()).apply {
+                border = CompoundBorder(
+                    EmptyBorder(0, 5, 5, 5),
+                    BevelBorder(BevelBorder.LOWERED)
+                )
+
+                add(JLabel("Account ID:"), GridBagConstraints().apply {
+                    gridx = 0
+                    gridy = 0
+                })
+                add(JLabel("123456789"), GridBagConstraints().apply {
+                    gridx = 1
+                    gridy = 0
+                })
+
+                add(JLabel("Account Hash:"), GridBagConstraints().apply {
+                    gridx = 0
+                    gridy = 1
+                })
+                add(JLabel("123456789"), GridBagConstraints().apply {
+                    gridx = 1
+                    gridy = 1
+                })
+
+                add(JLabel("Position Configuration:"), GridBagConstraints().apply {
+                    gridx = 0
+                    gridy = 2
+                })
+
+                add(
+                    JScrollPane(
+                        JTable(
+                            arrayOf(
+                                arrayOf("SPY", "35 %"),
+                                arrayOf("VTA", "35 %"),
+                                arrayOf("VNI", "15 %"),
+                                arrayOf("SFY", "15 %")
+                            ),
+                            arrayOf("Position", "Allocation")
+                        ).apply {
+
+                        }), GridBagConstraints().apply {
+                        gridy = 3
+                        gridwidth = 2
+                        weighty = 1.0
+                        weightx = 1.0
+                        fill = GridBagConstraints.BOTH
+                    })
+            }, GridBagConstraints().apply {
+                gridx = 1
+                gridy = 0
+                weightx = 3.0
+                weighty = 1.0
+                fill = GridBagConstraints.BOTH
+            })
+
             addWindowListener(object : WindowAdapter() {
                 override fun windowClosing(e: WindowEvent?) {
                     logger.trace { "Dialog is closing" }
@@ -29,7 +108,7 @@ abstract class ManageAccounts : JDialog(null as? Frame, "Manage Accounts", true)
     }
 
     fun showDialog() {
-        pack()
+//        pack()
         setLocationRelativeTo(null)
         isVisible = true
     }

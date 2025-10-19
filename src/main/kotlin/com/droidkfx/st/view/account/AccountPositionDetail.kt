1371f27e8778313fde9b4b50c446faca4abbb31e
droidkfx <kam157190@gmail.com>
2025-10-19 18:35:19 -0500

    Introduce editable table model in `AccountPositionDetail` for position targets
    - Add `AccountPositionTableModelRow` and `AccountPositionRowModel` to manage position target data.
    - Replace static table with editable table model for dynamic allocation updates.
    - Enable "Save" button when data changes and apply listener to reset state on save.

--- src/main/kotlin/com/droidkfx/st/view/account/AccountPositionDetail.kt
+++ src/main/kotlin/com/droidkfx/st/view/account/AccountPositionDetail.kt
@@ -1,6 +1,7 @@
 package com.droidkfx.st.view.account
 
 import com.droidkfx.st.position.AccountPosition
+import com.droidkfx.st.position.PositionTarget
 import io.github.oshai.kotlinlogging.KotlinLogging
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
@@ -11,6 +12,69 @@ import javax.swing.JPanel
 import javax.swing.JScrollPane
 import javax.swing.JTable
 import javax.swing.border.EmptyBorder
+import javax.swing.table.AbstractTableModel
+
+private class AccountPositionTableModelRow(
+    var symbol: String? = null,
+    var allocationTarget: Double? = null
+) {
+    fun toPositionTarget() = symbol?.let { symbol ->
+        allocationTarget?.let { allocationTarget ->
+            PositionTarget(symbol, allocationTarget)
+        }
+    }
+}
+
+private class AccountPositionRowModel(val data: MutableList<AccountPositionTableModelRow>) : AbstractTableModel() {
+    init {
+        if (data.isEmpty()) {
+            data.add(AccountPositionTableModelRow())
+        }
+    }
+
+    override fun getColumnName(columnIndex: Int): String {
+        return when (columnIndex) {
+            0 -> "Position"
+            1 -> "Allocation"
+            else -> ""
+        }
+    }
+
+    override fun getRowCount(): Int {
+        return data.size
+    }
+
+    override fun getColumnCount(): Int {
+        return 2
+    }
+
+    override fun setValueAt(value: Any?, rowIndex: Int, columnIndex: Int) {
+        if (value == null) {
+            return
+        }
+        when (columnIndex) {
+            0 -> data[rowIndex].symbol = value.toString()
+            1 -> data[rowIndex].allocationTarget = value.toString().toDoubleOrNull() ?: data[rowIndex].allocationTarget
+        }
+        if (rowIndex == data.size - 1) {
+            data.add(AccountPositionTableModelRow())
+        }
+        fireTableDataChanged()
+    }
+
+    override fun getValueAt(rowIndex: Int, columnIndex: Int): String? {
+        return when (columnIndex) {
+            0 -> data[rowIndex].symbol
+            1 -> data[rowIndex].allocationTarget?.let {
+                "%.2f %%".format(it.times(100))
+            } ?: ""
+
+            else -> null
+        }
+    }
+
+    override fun isCellEditable(rowIndex: Int, columnIndex: Int): Boolean = true
+}
 
 abstract class AccountPositionDetail(acctData: AccountPosition) : JPanel(GridBagLayout()) {
     private val logger = KotlinLogging.logger {}
@@ -23,16 +87,28 @@ abstract class AccountPositionDetail(acctData: AccountPosition) : JPanel(GridBag
         addRow("Account Number:", acctData.Account.accountNumber, 2)
         addRow("Account Hash:", acctData.Account.accountNumberHash.truncateMiddle(), 3)
 
-        add(
-            JScrollPane(
-                JTable(
-                    acctData.positionTargets.map {
-                        arrayOf(it.symbol, "%.2f %%".format(it.allocationTarget * 100))
-                    }.toTypedArray(),
-                    arrayOf("Position", "Allocation")
-                ).apply {
+        val tableModel = AccountPositionRowModel(acctData.positionTargets.map {
+            AccountPositionTableModelRow(
+                it.symbol,
+                it.allocationTarget
+            )
+        }.toMutableList())
+        val saveButton = JButton("Save").apply {
+            isEnabled = false
+            addActionListener {
+                logger.trace { "Save button clicked" }
+                isEnabled = false
+            }
+        }
+        tableModel.apply {
+            addTableModelListener {
+                saveButton.isEnabled = true
+            }
+        }
 
-                }), GridBagConstraints().apply {
+        add(
+            JScrollPane(JTable(tableModel)),
+            GridBagConstraints().apply {
                 gridy = 5
                 gridwidth = 2
                 weighty = 1.0
@@ -42,9 +118,7 @@ abstract class AccountPositionDetail(acctData: AccountPosition) : JPanel(GridBag
 
         add(JPanel().apply {
             border = EmptyBorder(5, 0, 5, 0)
-            add(JButton("Save").apply {
-                isEnabled = false
-            })
+            add(saveButton)
         }, GridBagConstraints().apply {
             gridx = 0
             gridy = 6

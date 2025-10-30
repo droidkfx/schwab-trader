ac5eb52ee88d5866175992692e480062db056e41
droidkfx <kam157190@gmail.com>
2025-10-30 11:38:27 -0500

    Implement `previewOrder` in `OrderService` using `OrdersClient` integration

--- src/main/kotlin/com/droidkfx/st/orders/OrderService.kt
+++ src/main/kotlin/com/droidkfx/st/orders/OrderService.kt
@@ -2,10 +2,11 @@ package com.droidkfx.st.orders
 
 import com.droidkfx.st.account.Account
 import com.droidkfx.st.schwab.client.OrdersClient
+import com.droidkfx.st.schwab.client.PreviewOrder
 import com.droidkfx.st.strategy.PositionRecommendation
 
-class OrderService(ordersClient: OrdersClient) {
-    fun previewOrder(account: Account, recommendation: PositionRecommendation) {
-        TODO("Not yet implemented")
+class OrderService(private val ordersClient: OrdersClient) {
+    fun previewOrder(account: Account, recommendation: PositionRecommendation): PreviewOrder? {
+        return ordersClient.previewOrder(account, recommendation).data
     }
 }

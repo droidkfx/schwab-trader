ec5709fae6210de79a010b0d40ae87611f7d3f81
droidkfx <kam157190@gmail.com>
2025-11-03 12:13:57 -0600

    Add `OrderModule` with initialization logging

---
+++ src/main/kotlin/com/droidkfx/st/order/OrderModule.kt
@@ -0,0 +1,11 @@
+package com.droidkfx.st.order
+
+import io.github.oshai.kotlinlogging.KotlinLogging.logger
+
+class OrderModule {
+    private val logger = logger {}
+
+    init {
+        logger.trace { "Initializing" }
+    }
+}
